24dd34925863c5a9fe95fb960bc3ee2e2e550ef9
Marko Zub <mzub@n-ix.com>
2017-01-24 15:36:37 +0200

    added prisme for highlight

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+(function() {
+	function initSnippet() {
+		// var code = document.getElementsByTagName('code');
+		// for (var i = 0; i < code.length; i++) {
+		// 	console.log(code[i]);
+		// 	hljs.highlightBlock(code[i]);
+		// }
+		// hljs.initHighlighting();
+	}
+
+	addEventListener('load', initSnippet);
+})();
